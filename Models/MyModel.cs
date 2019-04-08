4afbbd08fc4dc9bc980a460813e24c357bbe8310
Hari Prasath Manokaran <38764929+hari5719@users.noreply.github.com>
2019-04-08 12:28:57 -0700

    adding initial files

---
+++ Models/MyModel.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace VirtualPetDojoDatchi.Models
+{
+    public class Pet
+    {
+       public int Happiness {get;set;} = 20;
+
+       public int Fullness {get;set;} = 20;
+
+       public int Engery{get;set;} = 50; 
+
+       public int Meal{get;set;} = 3;
+
+    }
+}
